e8cbf55b1889f26532010fa2ae5a397c5287a100
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-15 11:22:58 +0700

    Add files via upload

---
+++ excersice1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+public class Atom //Tạo lớp Atom có các thuộc tính sau
+{
+    private int atomicNumber;//số hiệu nguyên tử
+    private string symbol;//ký hiệu
+    private string fullName;//tên đầy đủ
+    private double atomicWeight;//khối lượng nguyên tử(nguyên tử khối)
+
+    public bool Accept()
+    {
+        Console.Write("Enter atomic number: ");//hiện ra màn hình: nhập số hiệu nguyên tử
+        string input = Console.ReadLine();//đọc chuỗi vào và lưu vào biến input(dạng ký tự)
+
+        //kiểm tra đàu vào xem biến input có phải số nguyên hay không
+        if (!int.TryParse(input, out atomicNumber))
+        {
+            return false;
+        }
+
+        Console.Write("Enter symbol: ");//nhập ký hiệu nguyên tử
+        symbol = Console.ReadLine().Trim();//đọc chuỗi vào và lưu vào biên symbol,trong đó hàm .Trim() dùng để loại bỏ khoảng trắng ở đầu và cuối chuỗi
+        //kiểm tra biến symbol xem có rỗng không
+        if (symbol.Length == 0)
+        {
+            return false;
+        }
+
+        Console.Write("Enter full name: ");//nhập tên đầy đủ của nguyên tử
+        fullName = Console.ReadLine().Trim();//đọc chuỗi vào và lưu vào biên fullName,trong đó phương thức .Trim() dùng để loại bỏ khoảng trắng ở đầu và cuối chuỗi
+
+        //kiểm tra biến fullName xem có rỗng không
+        if (fullName.Length == 0)
+        {
+            return false;
+        }
+
+        Console.Write("Enter atomic weight: ");//hiện ra màn hình: nhập nguyên tử khối
+        input = Console.ReadLine();//đọc chuỗi vào và lưu vào biến input
+
+        //kiểm tra đàu vào xem biến input có phải số thực hay không, nếu có thì lưu biến vào atomicWeight
+        if (!double.TryParse(input, out atomicWeight))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /*Phương thức Display() sẽ hiển thị thông tin về nguyên tử trên đầu ra tiêu chuẩn,
+    bao gồm các thông tin về số hiệu nguyên tử, ký hiệu nguyên tử, tên đầy đủ của nguyên tử và khối lượng nguyên tử.*/
+    public void Display()
+    {
+        Console.WriteLine("{0,-4} {1,-3} {2,-15} {3,-10:F3}", atomicNumber, symbol, fullName, atomicWeight);//chỉnh hiển thị độ rộng của các ký tự
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Atom[] atoms = new Atom[10];//khai báo mảng có 10 phần tử
+        int numAtoms = 0;//giá trị ban đàu
+
+        Console.WriteLine("Atomic Information");
+        Console.WriteLine("==================");
+
+
+        /*vòng lặp cho 10 phần tử , Trong mỗi vòng lặp, một đối tượng Atom mới được tạo và thông tin của nó được chấp nhận thông qua hàm Accept().
+        Nếu accept() trả về giá trị false, vòng lặp sẽ được dừng lại*/
+        while (numAtoms < 10)
+        {
+            atoms[numAtoms] = new Atom();
+            if (!atoms[numAtoms].Accept())
+            {
+                break;
+            }
+            numAtoms++;
+        }
+
+        Console.WriteLine("No  Sym Name            Weight");
+        Console.WriteLine("------------------------------------");
+
+        //hiện ra màn hình mảng atoms
+        for (int i = 0; i < numAtoms; i++)
+        {
+            atoms[i].Display();
+        }
+    }
+}
---
+++ excersise2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Employee //Tạo lớp Employee có các thuộc tính sau
+{
+    private string firstName;
+    private string lastName;
+    private string address;
+    private long sin;
+    private double salary;
+
+    //tạo constructor
+    public Employee(string firstName, string lastName, string address, long sin, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.sin = sin;
+        this.salary = salary;
+    }
+
+    //tạo phương thức tính tiền thưởng của nhân viên với đối số là percentage
+    public double tienThuong(double percentage)
+    {
+        return salary * percentage;
+    }
+
+    //tạo phương thức Tostring() ghì đè lên phương thức có sẵn trong Employee, phương thức này sễ hiển thị thông tin như sau
+    public override string ToString()
+    {
+        return string.Format("Employee Name: {0} {1}\nAddress: {2}\nSIN: {3}\nSalary: {4:C}", firstName, lastName, address, sin, salary);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp = new Employee("Son", "Tran", "21 Jumpstreet", 514146347, 50000.00);//tạo một đối tượng Employee mới có tên emp
+
+        Console.WriteLine(emp.ToString());//in ra màn hình thông tin của emp
+
+        double bonus = emp.tienThuong(0.1); //tính tiền thưởng thêm với 0.1 phần trăm
+        Console.WriteLine("Bonus: {0:C}", bonus); //in ra màn hình tiền thưởng
+    }
+}
